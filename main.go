74c527a0450f9d1389caf366b5465197f8dbbf07
SHYCOPATH <devlinnich@gmail.com>
2023-11-20 16:41:47 +0700

    fix(config): main

--- main.go
+++ main.go
@@ -1,17 +1,15 @@
 package main
 
 import (
-	"fmt"
 	"lokasani/app/drivers/config"
 	"lokasani/app/drivers/mysql"
 	routes "lokasani/app/router"
-
 )
 
 func main() {
-    appConfig, dbConfig := config.InitConfig()
-    db := mysql.StartDB(dbConfig)
-    e := routes.Route(db)
+	_, dbConfig := config.InitConfig()
+	db := mysql.StartDB(dbConfig)
+	e := routes.Route(db)
 
-    e.Logger.Fatal(e.Start(fmt.Sprintf(":%d", appConfig.APP_PORT)))
+	e.Logger.Fatal(e.Start(":8080"))
 }
