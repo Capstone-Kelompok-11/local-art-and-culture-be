040a745e455aed3b5f381f14645046ec0a62f4dc
Anggi Pohontu <anggipohontu378@gmail.com>
2023-11-30 22:31:28 +0800

    add logger

--- main.go
+++ main.go
@@ -4,12 +4,19 @@ import (
 	"lokasani/app/drivers/config"
 	"lokasani/app/drivers/mysql"
 	routes "lokasani/app/router"
+
+	"github.com/labstack/echo/middleware"
 )
 
 func main() {
 	_, dbConfig := config.InitConfig()
 	db := mysql.StartDB(dbConfig)
 	e := routes.Route(db)
+	e.Use(middleware.CORS())
 
+	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format: "method=${method}, uri=${uri}, status=${status}\n",
+	  }))
+	  
 	e.Logger.Fatal(e.Start(":8080"))
 }
