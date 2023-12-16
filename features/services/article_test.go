0b136851a07ccec2672ed07ea7cf4d060d0881a3
Anggi Pohontu <anggipohontu378@gmail.com>
2023-12-16 14:19:55 +0800

    add testing article

--- features/services/article_test.go
+++ features/services/article_test.go
@@ -8,7 +8,6 @@ import (
 	"testing"
 
 	"github.com/golang/mock/gomock"
-	"github.com/stretchr/testify/assert"
 )
 
 func TestCreateArticle_Success(t *testing.T) {
@@ -67,26 +66,187 @@ func TestCreateArticle_Failure(t *testing.T) {
     }
 }
 
-func TestDeleteArticleSuccess(t *testing.T) {
-    ctrl := gomock.NewController(t)
-    defer ctrl.Finish()
+func TestDeleteArticle_Success(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
 
-    mockArticleRepository := mocks.NewMockIArticleRepository(ctrl)
-    mockArticleService := mocks.NewMockIArticleService(ctrl)
+	mockRepo := mocks.NewMockIArticleRepository(ctrl)
 
-    expectedArticle := response.Article{
-        Title:   "test",
-        Content: "test",
-        Status:  "true",
-    }
+	mockRepo.EXPECT().DeleteArticle(gomock.Any()).Return(response.Article{}, nil)
 
-    mockArticleRepository.EXPECT().DeleteArticle(gomock.Any()).Return(expectedArticle, nil)
-    gomock.InOrder(
-        mockArticleService.EXPECT().DeleteArticle(gomock.Any()).AnyTimes(),
-    )
+	article, err := mockRepo.DeleteArticle("articleID")
 
-    article, err := mockArticleService.DeleteArticle("articleID")
+	if err != nil {
+		t.Errorf("Expected no error, got %v", err)
+	}
 
-    assert.Nil(t, err)
-    assert.Equal(t, expectedArticle, article)
+	if article.Id != 0 {
+		t.Errorf("Expected empty article ID, got %d", article.Id)
+	}
+}
+
+func TestDeleteArticle_Failure(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockRepo := mocks.NewMockIArticleRepository(ctrl)
+
+	expectedError := errors.New("some error")
+	mockRepo.EXPECT().DeleteArticle(gomock.Any()).Return(response.Article{}, expectedError)
+
+	article, err := mockRepo.DeleteArticle("articleID")
+
+	if err != expectedError {
+		t.Errorf("Expected error %v, got %v", expectedError, err)
+	}
+
+	if article.Id != 0 {
+		t.Errorf("Expected empty article ID, got %d", article.Id)
+	}
+}
+
+func TestGetAllArticle_Success(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockService := mocks.NewMockIArticleService(ctrl)
+
+	expectedArticles := []response.Article{{Id: 1, Title: "Article 1"}, {Id: 2, Title: "Article 2"}}
+	expectedCount := 2
+	mockService.EXPECT().GetAllArticle(gomock.Any(), gomock.Any(), gomock.Any()).Return(expectedArticles, expectedCount, nil)
+
+	articles, count, err := mockService.GetAllArticle("category", 10, 1)
+
+	if err != nil {
+		t.Errorf("Expected no error, got %v", err)
+	}
+
+	if count != expectedCount {
+		t.Errorf("Expected count %d, got %d", expectedCount, count)
+	}
+
+	if len(articles) != len(expectedArticles) {
+		t.Errorf("Expected %d articles, got %d", len(expectedArticles), len(articles))
+	}
+}
+
+func TestGetAllArticle_Failure(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockService := mocks.NewMockIArticleService(ctrl)
+
+	expectedError := errors.New("some error")
+	mockService.EXPECT().GetAllArticle(gomock.Any(), gomock.Any(), gomock.Any()).Return(nil, 0, expectedError)
+
+	articles, count, err := mockService.GetAllArticle("category", 10, 1)
+
+	if err != expectedError {
+		t.Errorf("Expected error %v, got %v", expectedError, err)
+	}
+
+	if count != 0 {
+		t.Errorf("Expected count 0, got %d", count)
+	}
+
+	if articles != nil {
+		t.Errorf("Expected nil articles, got %v", articles)
+	}
+}
+
+func TestGetTrendingArticle_Success(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockService := mocks.NewMockIArticleService(ctrl)
+
+	expectedArticles := []response.Article{{Id: 1, Title: "Article 1"}, {Id: 2, Title: "Article 2"}}
+	expectedCount := 2
+	mockService.EXPECT().GetTrendingArticle(gomock.Any(), gomock.Any(), gomock.Any()).Return(expectedArticles, expectedCount, nil)
+
+	articles, count, err := mockService.GetTrendingArticle("category", 10, 1)
+
+	if err != nil {
+		t.Errorf("Expected no error, got %v", err)
+	}
+
+	if count != expectedCount {
+		t.Errorf("Expected count %d, got %d", expectedCount, count)
+	}
+
+	if len(articles) != len(expectedArticles) {
+		t.Errorf("Expected %d articles, got %d", len(expectedArticles), len(articles))
+	}
+}
+
+func TestGetTrendingArticle_Failure(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockService := mocks.NewMockIArticleService(ctrl)
+
+	expectedError := errors.New("some error")
+	mockService.EXPECT().GetTrendingArticle(gomock.Any(), gomock.Any(), gomock.Any()).Return(nil, 0, expectedError)
+
+	articles, count, err := mockService.GetTrendingArticle("category", 10, 1)
+
+	if err != expectedError {
+		t.Errorf("Expected error %v, got %v", expectedError, err)
+	}
+
+	if count != 0 {
+		t.Errorf("Expected count 0, got %d", count)
+	}
+
+	if articles != nil {
+		t.Errorf("Expected nil articles, got %v", articles)
+	}
+}
+
+func TestUpdateArticle_Success(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockService := mocks.NewMockIArticleService(ctrl)
+
+	expectedUpdatedArticle := response.Article{Id: 1, Title: "Updated Article"}
+	mockService.EXPECT().UpdateArticle(gomock.Any(), gomock.Any()).Return(expectedUpdatedArticle, nil)
+
+	updatedArticle, err := mockService.UpdateArticle("articleID", request.Article{Title: "New Title"})
+
+	if err != nil {
+		t.Errorf("Expected no error, got %v", err)
+	}
+
+	if updatedArticle.Id != expectedUpdatedArticle.Id {
+		t.Errorf("Expected article ID %d, got %d", expectedUpdatedArticle.Id, updatedArticle.Id)
+	}
+
+	if updatedArticle.Title != expectedUpdatedArticle.Title {
+		t.Errorf("Expected article title %s, got %s", expectedUpdatedArticle.Title, updatedArticle.Title)
+	}
+}
+
+func TestUpdateArticle_Failure(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockService := mocks.NewMockIArticleService(ctrl)
+
+	expectedError := errors.New("some error")
+	mockService.EXPECT().UpdateArticle(gomock.Any(), gomock.Any()).Return(response.Article{}, expectedError)
+
+	updatedArticle, err := mockService.UpdateArticle("articleID", request.Article{Title: "New Title"})
+
+	if err != expectedError {
+		t.Errorf("Expected error %v, got %v", expectedError, err)
+	}
+
+	if updatedArticle.Id != 0 {
+		t.Errorf("Expected empty article ID, got %d", updatedArticle.Id)
+	}
+
+	if updatedArticle.Title != "" {
+		t.Errorf("Expected empty article title, got %s", updatedArticle.Title)
+	}
 }
